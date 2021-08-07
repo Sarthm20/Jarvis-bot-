49179ef5a8a72175ff0d529bcba173da5dfe6655
Sarthm20 <77632098+Sarthm20@users.noreply.github.com>
2021-08-07 16:34:49 +0530

    Update jarvis.py

--- jarvis.py
+++ jarvis.py
@@ -72,17 +72,13 @@ if __name__ == "__main__":
         elif 'open google' in query:
             webbrowser.open("google.com")
 
-        elif 'open stackoverflow' in query:
-            webbrowser.open("stackoverflow.com")   
-
+       
 
 
         elif 'the time' in query:
             strTime = datetime.datetime.now().strftime("%H:%M:%S")    
             speak(f"Sir, the time is {strTime}")
 
-        elif 'open code' in query:
-            codePath = "C:\\Users\\Haris\\AppData\\Local\\Programs\\Microsoft VS Code\\Code.exe"
-            os.startfile(codePath)
+     
 
       
